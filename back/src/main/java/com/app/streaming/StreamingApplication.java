a2b25f7e7410146c93198416a13f1dbfab0e4a2c
pridato <davidarroyo25052004@gmail.com>
2024-11-09 18:28:47 +0100

    creada base de datos conectada con spring

--- back/src/main/java/com/app/streaming/StreamingApplication.java
+++ back/src/main/java/com/app/streaming/StreamingApplication.java
@@ -2,11 +2,22 @@ package com.app.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class StreamingApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// cargamos las variables de entorno para leerlas en application properties
+		System.setProperty("SPRING_DATASOURCE_URL", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_URL")));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_USERNAME")));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_PASSWORD")));
+		System.setProperty("SPRING_APPLICATION_NAME", Objects.requireNonNull(dotenv.get("SPRING_APPLICATION_NAME")));
+
 		SpringApplication.run(StreamingApplication.class, args);
 	}
 
---
+++ devops/ddbb/tablas.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE streaming_db;
+USE streaming_db;
+
+CREATE TABLE user (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  name varchar(50) not null,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password_hash VARCHAR(255) NOT NULL,
+  phone numeric(9) not null,
+  is_verified BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE video (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(255) NOT NULL,
+   description TEXT,
+   url VARCHAR(500) NOT NULL,
+   uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   user_id BIGINT NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comment (
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     content TEXT NOT NULL,
+     commented_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     user_id BIGINT NOT NULL,
+     video_id BIGINT NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+     FOREIGN KEY (video_id) REFERENCES video(id) ON DELETE CASCADE
+);
